31fdb99ba4c267758e399ffe3214f86eae82fa8d
Raphael Marques <raphaeljcmarques@gmail.com>
2022-08-03 20:58:40 -0300

    wip-feat: Countdown Component

--- src/components/Countdown/index.tsx
+++ src/components/Countdown/index.tsx
@@ -1,19 +1,45 @@
+import { useEffect, useState } from 'react';
 import * as S from './styles';
 
 export function Countdown() {
-  return (
-    <S.CountdownContainer>
-      <div>
-        <span>2</span>
-        <span>5</span>
-      </div>
-      <span>:</span>
-      <div>
-        <span>0</span>
-        <span>0</span>
-      </div>
+  const [time, setTime] = useState(25 * 60); // 25min in seconds
+  const [active, setActive] = useState(false);
 
-      <button type="button">Iniciar um ciclo</button>
-    </S.CountdownContainer>
+  const minutes = Math.floor(time / 60);
+  const seconds = time % 60;
+
+  const [minuteLeft, minuteRight] = String(minutes).padStart(2, '0').split('');
+  const [secondLeft, secondRight] = String(seconds).padStart(2, '0').split('');
+
+  function startCountdown() {
+    setActive(true);
+  }
+
+  useEffect(() => {
+    if (active && time > 0) {
+      setTimeout(() => {
+        setTime(time - 1);
+      }, 1000);
+    }
+  }, [active, time]);
+
+  return (
+    <div>
+      <S.CountdownContainer>
+        <div>
+          <span>{minuteLeft}</span>
+          <span>{minuteRight}</span>
+        </div>
+        <span>:</span>
+        <div>
+          <span>{secondLeft}</span>
+          <span>{secondRight}</span>
+        </div>
+      </S.CountdownContainer>
+
+      <S.CountdownButton type="button" onClick={startCountdown}>
+        Iniciar um ciclo
+      </S.CountdownButton>
+    </div>
   );
 }
--- src/components/Countdown/styles.ts
+++ src/components/Countdown/styles.ts
@@ -38,3 +38,29 @@ export const CountdownContainer = styled.div`
     }
   }
 `;
+
+export const CountdownButton = styled.button`
+  width: 100%;
+  height: 5rem;
+
+  margin-top: 2rem;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  border: 0;
+  border-radius: 5px;
+
+  background-color: ${props => props.theme.blue};
+  color: ${props => props.theme.white};
+
+  font-size: 1.25rem;
+  font-weight: 600;
+
+  transition: background-color 0.2s;
+
+  &:hover {
+    background-color: ${props => props.theme['blue-dark']};
+  }
+`;
